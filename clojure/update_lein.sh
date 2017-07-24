80dc4bc8e9803e319b432515a0984f2bfd021c13
Michael West <quagly@gmail.com>
2017-07-24 12:52:41 -0700

    removed work arounds for work proxy for clojure

--- clojure/update_lein.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# can't get to this link from work so storing lein script locally
-# it is http but resoves to https and I can't get through the proxy
-curl -L -s http://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > lein

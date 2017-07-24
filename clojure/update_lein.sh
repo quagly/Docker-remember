be13d09f669336d44dc42d6c19d86bab4b0c58ff
Michael West <quagly@gmail.com>
2017-07-24 11:51:33 -0700

    fixing clojure lein from work

---
+++ clojure/update_lein.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# can't get to this link from work so storing lein script locally
+# it is http but resoves to https and I can't get through the proxy
+curl -L -s http://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > lein

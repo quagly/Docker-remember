1a5add98d6c181dfb7f375ec312cd0f5c5d218cd
Michael West <quagly@gmail.com>
2017-10-23 10:57:35 -0700

    published mypostgres image to dockerhub

--- postgres/init-user-db.sh
+++ postgres/init-user-db.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# for sql where command order matter it is best to using shell script
+# there does not seem to be a docker mechanism to run sql in sort order
+# however, we could easily modify the /docker-entrypoint.sh script to do this
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER docker;
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+    CREATE ROLE poc WITH LOGIN PASSWORD 'poc';
+    CREATE DATABASE poc;
+    GRANT ALL PRIVILEGES ON DATABASE poc TO poc;
 EOSQL
--- postgres/start.sh
+++ postgres/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # start up the container and initialize the database
 # so all local processes can connect
-docker run --name postgrestest -e POSTGRES_PASSWORD=test -d mypostgres
+
+#docker run --name postgrestest -e POSTGRES_PASSWORD=test -d mypostgres
+
+# expose port 5432 to local port 5432
+# note assuming default bridge network
+docker run -p 5432:5432 --name postgrestest -d mypostgres

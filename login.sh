c77e63fb6c4b4794fd8131b3102668454deb2ecb
Michael West <quagly@gmail.com>
2018-01-07 04:02:54 -0800

    changed from volume to mount flag for docker run

--- login.sh
+++ login.sh
@@ -2,4 +2,28 @@
 
 # login shell
 # mount ssh directory for keys
-docker run --volume ~/.ssh:/home/developer/.ssh --rm -it full bash -il
+# I'm naming the container now since I only every use one
+
+# example volume syntax
+# mounts are read only since I don't want to mess up the host from the container
+#docker run \
+#  --volume ~/.ssh:/home/developer/.ssh:ro \
+#  --volume ~/.aws:/home/developer/.aws:ro \
+#  --name full-running \
+#  --rm -it \
+#  full \
+#  bash -il
+
+# example mount syntax which is the newer more flexible way
+# though it doesn't really matter since we are only using bind mounts
+docker run \
+  --name full-running \
+  --mount type=bind,source=${HOME}/.ssh,target=/home/developer/.ssh,readonly \
+  --mount type=bind,source=${HOME}/.aws,target=/home/developer/.aws,readonly \
+  --rm -it \
+  full \
+  bash -il
+
+
+
+
